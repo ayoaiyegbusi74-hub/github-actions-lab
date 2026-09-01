d838b83a970ff9bb142012327d2dcf463ab664a5
ayoaiyegbusi74-hub <ayoaiyegbusi74@gmail.com>
2026-09-01 10:35:59 -0500

    Add Terraform Docker infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = "~> 1.15"
+
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 4.2.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  name    = "terraform-nginx-lab"
+  image   = docker_image.nginx.image_id
+  restart = "unless-stopped"
+
+  ports {
+    internal = 80
+    external = 8081
+  }
+}
+
+output "service_url" {
+  value = "http://localhost:8081"
+}
